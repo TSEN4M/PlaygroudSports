571f74f7d1971228faa903e9d32040eea3a56def
TSEN4M <deathnotefreak05@gmail.com>
2024-06-12 16:16:31 +0530

    changed name

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import './App.css'
+import Header from '.src/components/Header.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ function Header() {
   return (
     <>
         <header>
+        <h1>The Playground Sports</h1>
             <nav>
                 <ul>
                     <li>Upcoming Events</li>
