0a3153c38086a17e2113c357def2ce34984fe467
Dileep Kumar <dileepvaltheti@gmail.com>
2024-06-12 16:59:23 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
-<<<<<<< HEAD
 import React from 'react';
 import LandingPage from './components/LandingPage.jsx';
-import './App.css';
-=======
-import { useState } from 'react'
-import './App.css'
-import Header from '.src/components/Header.jsx'
-
-function App() {
- 
->>>>>>> 571f74f7d1971228faa903e9d32040eea3a56def
-
 const App = () => {
   return (
-<<<<<<< HEAD
     <div className="App">
       <LandingPage />
     </div>
   );
 };
-=======
-    <>
-      <Header/>
-    </>
-  )
-}
->>>>>>> 571f74f7d1971228faa903e9d32040eea3a56def
 
 export default App;
